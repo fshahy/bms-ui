b8c09706f33fef6996cb4cb692a141266d10cea4
Farid Shahy <fshahy@gmail.com>
2023-12-10 16:06:50 +0100

    add products and customers views

---
+++ src/views/CustomersView.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        data() {
+            return {
+                customers: []
+            }
+        },
+
+        computed() {
+        },
+
+        created() {
+            fetch("http://localhost:8080/customers")
+                .then(response => response.json())
+                .then(data => this.customers = data)
+        }
+    }
+</script>
+
+<template>
+    <table class="border border-solid table-auto">
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>Name</td>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr v-for="customer in customers" v-bind:key="customer.id">
+                <td>{{ customer.id }}</td>
+                <td>{{ customer.name }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>

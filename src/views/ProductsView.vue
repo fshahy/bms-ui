837f3b9c0f114e8acb1aab21597d4b33d7f18019
Farid Shahy <fshahy@gmail.com>
2023-12-11 22:04:13 +0100

    add form view for products page

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -2,7 +2,10 @@
     export default {
         data() {
             return {
-                products: []
+                products: [],
+                listView: true,
+                formView: false,
+                currentProduct: {},
             }
         },
 
@@ -13,26 +16,78 @@
             fetch("http://localhost:8080/products")
                 .then(response => response.json())
                 .then(data => this.products = data);
-        }
+        },
+
+        methods: {
+            showForm(product) {
+                this.listView = false;
+                this.formView = true;
+                this.currentProduct = product;
+            },
+            showList() {
+                this.formView = false;
+                this.listView = true;
+            }
+        },
+
     }
 </script>
 
 <template>
-    <table class="border border-solid table-auto">
-        <thead>
-            <tr>
-                <td>ID</td>
-                <td>Code</td>
-                <td>Name</td>
-            </tr>
-        </thead>
+    <div v-show="listView">
+        <div class="p-2    bg-black text-white flex space-x-4">
+            <button>New</button>
+            <button>Search</button>
+        </div>
+        <hr />
+        <table  class="w-full p-8 border border-solid table-auto">
+            <thead class="bg-gray-200">
+                <tr class="font-bold">
+                    <td>ID</td>
+                    <td>Code</td>
+                    <td>Name</td>
+                </tr>
+            </thead>
 
-        <tbody>
-            <tr v-for="product in products" v-bind:key="product.it">
-                <td>{{ product.id }}</td>
-                <td>{{ product.code }}</td>
-                <td>{{ product.name }}</td>
-            </tr>
-        </tbody>
-    </table>
+            <tbody>
+                <tr v-for="product in products" v-bind:key="product.id" @click="this.showForm(product)" class="border border-solid hover:bg-sky-700">
+                    <td>
+                        <div class="underline">{{ product.id }}</div>
+                    </td>
+                    <td>
+                        <div class="underline">{{ product.code }}</div>
+                    </td>
+                    <td>
+                        <div class="underline">{{ product.name }}</div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+
+    <div v-show="formView">
+        <div class="p-2 bg-black text-white flex space-x-4">
+            <button @click="this.showList" class="underline">Show List</button>
+            <button>Save</button>
+            <button>Delete</button>
+        </div>
+        <hr />
+        <table class="w-full">
+            <thead class="bg-gray-200">
+                <tr class="font-bold">
+                    <td>ID</td>
+                    <td>Code</td>
+                    <td>Name</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{ this.currentProduct.id }}</td>
+                    <td>{{ this.currentProduct.code }}</td>
+                    <td>{{ this.currentProduct.name }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>

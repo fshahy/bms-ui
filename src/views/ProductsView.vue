58e9079e04dcf92fd9d08b5d7dc45dd50dc29160
Farid Shahy <fshahy@gmail.com>
2023-12-12 00:30:27 +0100

    update form view. add checkbox in list view.

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -27,6 +27,11 @@
             showList() {
                 this.formView = false;
                 this.listView = true;
+            },
+            selectProduct(product, event) {
+                if(event) {
+                    event.stopPropagation();
+                }
             }
         },
 
@@ -34,15 +39,17 @@
 </script>
 
 <template>
-    <div v-show="listView">
+    <div v-if="listView">
         <div class="p-2    bg-black text-white flex space-x-4">
             <button>New</button>
             <button>Search</button>
+            <button>Delete</button>
         </div>
         <hr />
-        <table  class="w-full p-8 border border-solid table-auto">
+        <table  class="w-full p-8 mt-2 border border-solid table-auto">
             <thead class="bg-gray-200">
                 <tr class="font-bold">
+                    <td></td>
                     <td>ID</td>
                     <td>Code</td>
                     <td>Name</td>
@@ -51,13 +58,16 @@
 
             <tbody>
                 <tr v-for="product in products" v-bind:key="product.id" @click="this.showForm(product)" class="border border-solid hover:bg-sky-700">
-                    <td>
+                    <td class="p-1">
+                        <input @click="this.selectProduct(product, $event)" type="checkbox" name="" id="{{ product.id }}">
+                    </td>
+                    <td class="p-1">
                         <div class="underline">{{ product.id }}</div>
                     </td>
-                    <td>
+                    <td class="p-1">
                         <div class="underline">{{ product.code }}</div>
                     </td>
-                    <td>
+                    <td class="p-1">
                         <div class="underline">{{ product.name }}</div>
                     </td>
                 </tr>
@@ -66,28 +76,26 @@
     </div>
 
 
-    <div v-show="formView">
+    <div v-if="formView">
         <div class="p-2 bg-black text-white flex space-x-4">
             <button @click="this.showList" class="underline">Show List</button>
             <button>Save</button>
             <button>Delete</button>
         </div>
         <hr />
-        <table class="w-full">
-            <thead class="bg-gray-200">
-                <tr class="font-bold">
-                    <td>ID</td>
-                    <td>Code</td>
-                    <td>Name</td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{{ this.currentProduct.id }}</td>
-                    <td>{{ this.currentProduct.code }}</td>
-                    <td>{{ this.currentProduct.name }}</td>
-                </tr>
-            </tbody>
-        </table>
+
+        <div class="mt-2">
+            <label for="product_id" class="font-bold">ID:</label>
+            <input type="text" id="product_id" name="product_id" :value="this.currentProduct.id" disabled class="mx-2 border rounded">
+
+            <label for="product_code" class="font-bold">Code:</label>
+            <input type="text" id="product_code" name="product_code" :value="this.currentProduct.code" class="mx-2 border rounded">
+
+            <label for="product_name" class="font-bold">Name:</label>
+            <input type="text" id="product_name" name="product_name" :value="this.currentProduct.name" class="mx-2 border rounded">
+        </div>
     </div>
 </template>
+
+<style>
+</style>
